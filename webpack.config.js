fe521836c99a8a25cca7194f527cea96ecb2a35c
Marco Antonio Mejia Lopez <tomatin77@gmail.com>
2021-05-25 14:56:12 -0500

    se modifico webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,21 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin= require('terser-webpack-plugin');
-const {CleantWebpackPlugin} = require('clean-webpack-plugin');
-
-
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicpath: '/',
+    publicPath: "/",
   },
   resolve: {
     extensions: ['.js', '.jsx'],
     alias: {
       '@components': path.resolve(__dirname, 'src/components/'),
-      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@styles': path.resolve(__dirname, 'src/styles/')
     }
   },
   mode: 'production',
@@ -38,15 +36,14 @@ module.exports = {
         ]
       },
       {
-          test: /\.s[ac]ss$/,
-          use:[
-              'style-loader',
-              'css-loader',
-              'sass-loader'
-            ]
+        test: /\.s[ac]ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
       }
     ]
-
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -54,15 +51,15 @@ module.exports = {
       filename: './index.html'
     }),
     new MiniCssExtractPlugin({
-        filename: '[name].css'
+      filename: '[name].css'
     }),
-    new CleantWebpackPlugin(),
+    new CleanWebpackPlugin(),
   ],
-  optimization:{
-    minimize:true,
-    minimizer:[
+  optimization: {
+    minimize: true,
+    minimizer: [
       new CssMinimizerPlugin(),
-      new TerserPlugin()
+      new TerserPlugin(),
     ]
   }
 }
